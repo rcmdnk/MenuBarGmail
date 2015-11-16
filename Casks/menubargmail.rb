9bec51153ec0271aa8c89bae634336289163775a
rcmdnk <rcmdnk@gmail.com>
2015-11-16 09:17:17 +0900

    updated formula for app v0.0.1

--- Casks/menubargmail.rb
+++ Casks/menubargmail.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'menubargmail' do
   version '0.0.1'
-  sha256 ''
+  sha256 '881e1651443f18c79a75574173006eb3f00686f2e78fffe83e42317cda45b3c5'
 
-  url '#{version}.zip'
+  url "https://github.com/rcmdnk/MenuBarGmail/archive/MenuBarGmail_v#{version}.zip"
   name 'MenuBarGmail'
   homepage 'https://github.com/rcmdnk/MenuBarGmail'
   license :mit
